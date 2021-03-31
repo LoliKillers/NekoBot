eeb38d79889a65459b537a41a73e67ffcc6a422c
002 <80396688+Zer0-Tw0@users.noreply.github.com>
2021-03-31 17:51:55 +0700

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,7 +1,7 @@
 class Config(object):
     LOGGER = True
     TOKEN = "1457068950:AAFunUFSFW2IBT9rgjbvqyL3lvB1cbnzSis"
-    MONGO_URI = "mongodb+srv://Arisusanto157:<password>@cluster0.c11xf.mongodb.net/<dbname>?retryWrites=true&w=majority"
+    MONGO_URI = "mongodb+srv://username:<password>@cluster0.c11xf.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
 
 class Production(Config):
