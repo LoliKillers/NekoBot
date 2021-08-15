2b4cac5959da869405de600a11853e5fbdba5cb3
Loli Killers <80396688+LoliKillers@users.noreply.github.com>
2021-08-15 18:46:16 +0700

    Update config.py

--- Bot/config.py
+++ Bot/config.py
@@ -1,6 +1,6 @@
 class Config(object):
     LOGGER = True
-    TOKEN = "1457068950:AAFunUFSFW2IBT9rgjbvqyL3lvB1cbnzSis"
+    TOKEN = "1870815445:AAEODGdC2uvoxw0vue4sKBeVxH6ZTmACnDw"
     MONGO_URI = "mongodb+srv://username:<password>@cluster0.c11xf.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
 
